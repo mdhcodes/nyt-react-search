b6e2e4186973355d034d594ed5387388c5196e04
Michelle Hartley <mdhcodes@gmail.com>
2017-06-11 17:42:31 -0400

    Add id for results component (results-well)

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -13,7 +13,7 @@ class Search extends React.Component {
           <div className="col-lg-10 col-lg-offset-1">
             <div className="panel panel-default">
               <div className="panel-heading text-center"><h3>Results</h3></div>
-              <div className="panel-body text-center">
+              <div className="panel-body text-center" id="results-well">
               </div>
             </div>
           </div>
